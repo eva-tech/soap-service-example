dcc6edd81781d63fe19e96e7e1db8a9ad43958e3
Jorge Luis Juarez <jorge.juarez@evacenter.com>
2023-04-25 09:54:16 -0600

    demo example

---
+++ SoapTest/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CallService();
+        Console.ReadLine();
+
+    }
+
+    private static async void CallService()
+    {
+        EvaSoap.pacs_soap_serviceClient client = new EvaSoap.pacs_soap_serviceClient(EvaSoap.pacs_soap_serviceClient.EndpointConfiguration.pacs_soap_service);
+
+
+
+        var tokenRequest = new EvaSoap.RequestHeader();
+        tokenRequest.email = "user@example.com";
+        tokenRequest.password = "M1P@$$word!";
+
+        var authMethod = new EvaSoap.authenticate();
+
+        var tokenResult = await client.authenticateAsync(tokenRequest, authMethod);
+        string token = tokenResult.authenticateResponse.authenticateResult.token;
+        DateTime expires = tokenResult.authenticateResponse.authenticateResult.expires;
+
+
+        Console.WriteLine(String.Format("Token: {0}, Expires: {1}", token, expires));
+        
+        
+    }
+}
